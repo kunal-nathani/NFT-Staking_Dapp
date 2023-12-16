a10f44a1715a61b11fdcf3203b9bd6dea3ffd8a2
Kunal Nathani <kunal.n@somaiya.edu>
2023-12-16 13:28:56 +0530

    .
    . . . .

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xc7e1610269Fe3B003503193582754C03bD11Bbd9";
+export const tokenContractAddress =
+  "0x48BF6755B5aB569ff980A3e9DF774567107e122e";
+export const stakingContractAddress =
+  "0x904cA0DE2b7e44A4c907cCe1651F5e8de1d43F96";
